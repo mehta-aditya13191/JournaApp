32ca585886faa2648a0cc7b6c8d602e6eea802e6
Aditya Mehta <aditya0mehta1@gmail.com>
2025-04-05 16:37:15 +0530

    Rest api

---
+++ journalApp/src/main/java/com/kmehta/journalApp/controller/JournalEntryController.java
@@ -0,0 +1,43 @@
+package com.kmehta.journalApp.controller;
+
+import com.kmehta.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController {
+
+    private Map<Long, JournalEntry> journalEntries = new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntry> getAll(){
+        return new ArrayList<>(journalEntries.values());
+    }
+
+    @PostMapping
+    public boolean createEntry(@RequestBody JournalEntry myEntry){
+            journalEntries.put(myEntry.getId(),myEntry);
+            return true;
+    }
+
+    @GetMapping("id/{myId}")
+    public JournalEntry getJournalById(@PathVariable Long myId){
+       return journalEntries.get(myId);
+    }
+
+    @DeleteMapping("id/{myId}")
+    public JournalEntry deleteJournalById(@PathVariable Long myId){
+        return journalEntries.remove(myId);
+    }
+
+    @PutMapping("/id/{id}")
+    public JournalEntry updateJournalById(@PathVariable Long id,@RequestBody JournalEntry myEntry){
+        return journalEntries.put(id,myEntry);
+    }
+
+}
